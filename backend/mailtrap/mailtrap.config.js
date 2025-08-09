ea234f9ec317dfe8c9621754efc2db99468f7b6f
jacobl3371 <jacobl161@nycstudents.net>
2025-08-09 01:03:48 -0400

    fix user createdAt date being invalid

--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const mailtrapClient = new MailtrapClient({
-	endpoint: process.env.MAILTRAP_ENDPOINT,
-	token: process.env.MAILTRAP_TOKEN,
+  token: process.env.MAILTRAP_TOKEN,
 });
 
 export const sender = {
-	email: "mailtrap@demomailtrap.com",
-	name: "Burak",
+  email: "hello@demomailtrap.co",
+  name: "Mailtrap Test",
 };
